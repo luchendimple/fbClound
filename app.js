71ee827eab041cb2e576cc547633fc7c97ee3bcd
luchendimple <69505915+luchendimple@users.noreply.github.com>
2020-08-13 21:06:17 +0800

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((err, req, res, next) => {
         error: {}
     });
 });
-app.set('port', process.env.PORT || 80);
+//app.set('port', process.env.PORT || 80);
 const server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
 });
